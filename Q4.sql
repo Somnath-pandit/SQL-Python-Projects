8ccb9cd8e4558f2a10d7f6eef46df95b9ac103bc
Somnath Pandit <86231309+Somnath-pandit@users.noreply.github.com>
2023-12-24 09:01:47 +0530

    Delete Q4.sql

--- Q4.sql
+++
@@ -1,7 +0,0 @@
--- 4. Retrieve a list of users along with their usernames and the 
--- rank of their account creation, ordered by the creation date in ascending order.
-
-SELECT *,
-		RANK() OVER (ORDER BY created_at) ac_creation_rank
-FROM users
-ORDER BY created_at
